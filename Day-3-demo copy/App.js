c263e7a1605c39c1f9de6ea506f0e4da7d2efeb9
deepika-panneerselvam <157014890+deepika-panneerselvam@users.noreply.github.com>
2024-01-24 13:46:21 +0530

    Delete Day-3-demo copy directory

--- Day-3-demo copy/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
- 
-let heading1 = document.createElement("H1");
-heading1.innerHTML = "Home Page";
-let root = document.getElementById("root");
-root.appendChild(heading1);
-
