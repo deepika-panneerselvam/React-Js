4f53c37c4539d64d519b278f3fb265f5ff301dfc
deepika-panneerselvam <deepika.panneerselvam@xebia.com>
2024-01-24 13:24:04 +0530

    first commit

---
+++ Day-3-demo/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+ 
+let heading1 = document.createElement("H1");
+heading1.innerHTML = "Home Page";
+let root = document.getElementById("root");
+root.appendChild(heading1);
+
---
+++ gg/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+ 
+const jsx=
+    <h1 className="heading">
+       heading
+    </h1>;
+
+    const root =ReactDOM.createRoot(document.getElementById("root"));
+    root.render(jsx);
